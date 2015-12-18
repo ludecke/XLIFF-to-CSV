02b5d002f4ef18bfd7f9fffbcf6d42a2f1dacb8a
papercranes <martin@ludecke.net>
2015-12-18 15:02:21 +0100

    Operational, but needs to be tested with more load

--- Convert_XLIFF_Export_to_CSV.py
+++ Convert_XLIFF_Export_to_CSV.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-#This script takes a proposal export file generated with RS_TRANSLATION_EXPORT_PP (4 character language keys as input and outputs a CSV file containing all proposals, exclusing exceptions.
+#This script takes XLIFF exports from a Unicode SAP system and outputs a CSV containing source text, target text, and domain.
+#Duplicates (source *and* target text are the same) are removed, but duplicates for target texts shorter than 5 characters are
+#only removed within domains, so one instance remains per domain.
+
 import os
 import re
 import codecs
 import sys
 import xml.etree.ElementTree as ET
-
-#sys.argv  #sys.argv[1] is the file to upload
-
 
 sourcelang = 0
 targetlang = 0
@@ -44,11 +44,27 @@ for filename in os.listdir(os.getcwd()+"/source"):
                     transunit.targetlang = file.get('target-language')
                     transunit.domain = file.get('category')
                     transunit.sourcetext = trans_unit_parameter[0].text
-                    transunit.targettext = trans_unit_parameter[1].text
-                    #print repr(transunit.sourcelang)
-                    #add to list
-                    translation_units.append(transunit)
-                    
+                    #set target text for red lines to standard value
+                    for target in trans_unit_parameter:
+                        if target.text is None:
+                            transunit.targettext = "this is actually a red line, don't add it"
+                        else:
+                            transunit.targettext = target.text
+                        
+                    #exlude supplemented lines and red lines
+                    if not transunit.sourcetext == transunit.targettext:
+                        if transunit.targettext != "this is actually a red line, don't add it":
+                    #exclude duplicates
+                    #if the source text is very short, allow 1 duplicates for each domain, since they will be created as exceptions in PP.
+                    #Modify the numer to change minimum length
+                            if len(transunit.sourcetext) > 4:
+                                if not any (s.targettext == transunit.targettext and s.sourcetext == transunit.sourcetext for s in translation_units):
+                                    translation_units.append(transunit)
+                    #if the source text longer, allow no duplicates, ignoring domains
+                            else:
+                                if not any (s.targettext == transunit.targettext and s.sourcetext == transunit.sourcetext and s.domain == transunit.domain for s in translation_units):
+                                    translation_units.append(transunit)
+
 #output CSV as UTF with BOM so MS Excel can read it
 with codecs.open ("exported_text_pairs.csv", 'w', encoding='utf-8_sig') as f:
     #add header
@@ -56,4 +72,3 @@ with codecs.open ("exported_text_pairs.csv", 'w', encoding='utf-8_sig') as f:
     for i in translation_units:
         #prepend u
         f.write (u"{};{};{};{};{};{}\n".format(i.sourcelang, i.targetlang, i.sourcetext, i.targettext, i.domain, i.objectname))
-        
